8c306f6437db944f27e5e499d12cb3f1e9b2fc4f
Marc <marcgreen@wpi.edu>
2022-04-06 01:22:57 +0200

    up to p.18 Talking to Docker

---
+++ test/Spec.hs
@@ -0,0 +1,38 @@
+module Main where
+
+import RIO
+import Core
+
+import qualified RIO.NonEmpty.Partial as NonEmpty.Partial
+
+-- helper funcs
+
+makeStep :: Text -> Text -> [Text] -> Step
+makeStep name image commands
+  = Step
+    { name = StepName name
+    , image = Image image
+    , commands = NonEmpty.Partial.fromList commands
+    }
+
+makePipeline :: [Step] -> Pipeline
+makePipeline steps =
+  Pipeline { steps = NonEmpty.Partial.fromList steps }
+
+-- test values
+
+testPipeline :: Pipeline
+testPipeline = makePipeline
+  [ makeStep "first step" "ubuntu" ["date"]
+  , makeStep "second step" "ubuntu" ["uname -r"]
+  ]
+
+testBuild :: Build
+testBuild = Build
+  { pipeline = testPipeline
+  , state = BuildReady
+  , completedSteps = mempty
+  }
+
+main :: IO ()
+main = pure ()
