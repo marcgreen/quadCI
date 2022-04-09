c722499cf75973e880804b83c234a5fb5e4ee2e0
Marc <marcgreen@wpi.edu>
2022-04-09 02:40:33 +0200

    up to pg. 43 Getting tests to fail

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,16 @@
 module Main where
 
 import RIO
+import qualified RIO.Map as Map
+import qualified RIO.NonEmpty.Partial as NonEmpty.Partial
+
+import qualified System.Process.Typed as Process
 
 import Core
 import qualified Docker
+import qualified Runner
 
-import qualified RIO.NonEmpty.Partial as NonEmpty.Partial
+import Test.Hspec
 
 -- helper funcs
 
@@ -21,20 +26,28 @@ makePipeline :: [Step] -> Pipeline
 makePipeline steps =
   Pipeline { steps = NonEmpty.Partial.fromList steps }
 
--- test values
+-- test cases
 
-testPipeline :: Pipeline
-testPipeline = makePipeline
-  [ makeStep "first step" "ubuntu" ["date"]
-  , makeStep "second step" "ubuntu" ["uname -r"]
-  ]
+testRunSuccess :: Runner.Service -> IO ()
+testRunSuccess runner = do
+  build <- runner.prepareBuild $ makePipeline
+    [ makeStep "First step" "ubuntu" ["date"]
+    , makeStep "Second step" "ubuntu" ["uname -r"]
+    ]
+  result <- runner.runBuild build
 
-testBuild :: Build
-testBuild = Build
-  { pipeline = testPipeline
-  , state = BuildReady
-  , completedSteps = mempty
-  }
+  result.state `shouldBe` BuildFinished BuildSucceeded
+  Map.elems result.completedSteps `shouldBe` [StepSucceeded,StepSucceeded]
 
 main :: IO ()
-main = pure ()
+main = hspec do
+  docker <- runIO Docker.createService
+  runner <- runIO $ Runner.createService docker
+  
+  beforeAll cleanupDocker $ describe "Quad CI" do
+    it "should run a build (success)" do
+      testRunSuccess runner
+
+cleanupDocker :: IO ()
+cleanupDocker = void do
+  Process.readProcessStdout "docker rm -f $(docker ps -aq --filter \"label=quad\")"
